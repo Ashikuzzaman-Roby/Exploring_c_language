99067cc8030f9678782bdb7f64ac77396abd343d
Ashikuzzaman Roby <mdroby3357@gmail.com>
2025-11-02 16:22:04 +0600

    String practice problem

--- string_in_c_01.c
+++ string_in_c_01.c
@@ -64,6 +64,102 @@ void reverse_func(char *s, int len){
 
 }
 
+void revemove_delimeter(char *ptr, int len ){
+    int count =0;
+    int idx=0;
+
+    for(int i=0;i<len;i++){
+        if(ptr[i]=='.'){
+            count+=1;
+        } 
+    }
+    printf("%d\n", count);
+    char *new_s = (char *)malloc((len-count) * sizeof(char));
+
+    if(new_s == NULL){
+        printf("Memory allocation failed \n");
+        return ;
+    }
+
+
+    
+    for(int i= 0 ; i<len;i++ ){
+        if(ptr[i]!='.'){
+            new_s[idx]=ptr[i];
+            idx+=1;
+
+        }
+        else{
+            continue;
+        }
+    }
+    new_s[idx]='\0';
+
+    printf("%s\n",new_s);
+
+
+    free(new_s);
+
+
+
+}
+
+void declare_result(char *ptr , int len){
+    char first_num[5];
+    char second_num[5];
+    int count_1 = 0 ;
+    int count_2 = 0 ;
+    int idx ; 
+    for(int i = 0 ; i<len ;i++){
+        if(ptr[i]=='-'){
+            idx = i;
+            break;
+        
+            
+        }
+    }
+    
+
+
+
+    for( int i = 0 ; i<idx; i++){
+        first_num[i]=ptr[i];
+        count_1+=1 ; 
+
+    }
+
+    first_num[count_1]='\0';
+
+
+    for(int i = idx+1 ; i<len ; i++){
+        second_num[count_2]=ptr[i];
+        count_2+=1;
+    }
+
+    second_num[count_2]='\0';
+
+    printf("%c\n", first_num[0]);
+    printf("%s\n", first_num);
+    printf("%s\n",second_num);
+    int num1 = atoi(first_num);
+    int num2 = atoi(second_num);
+    if(num1>num2){
+        int diff = num1-num2;
+        printf("Ronaldo's team has won the match by %d",diff);
+    }
+    else if(num1<num2){
+        int dif = num2-num1;
+        printf("Ronaldo's team has won the game %d",dif);
+    }
+    else{
+        printf("It's a draw !!");
+    }
+}
+
+
+
+
+
 int main(){
     printf("problem  no 01 : Palendromic in string \n");
     char s[] = "abba";
@@ -86,6 +182,24 @@ int main(){
     char s1[] = "i.like.this.program.very.much" ;
     char *str =strdup(s1);
     printf("copy at a time in c : %s\n", str);
+
+
+    // part 02 : 
+
+    char s5[] = "I.Love.python";
+    int len5 = sizeof(s5)/sizeof(s5[0]);
+    revemove_delimeter(s5 , len5);
+
+    // taking an input from the user ; 
+
+    char score[10];
+
+    printf("ENter The score of Realmadrid and bercalona : ");
+    scanf("%s",score);
+    int len6 =strlen(score);
+    declare_result(score , len6+1);
+
+
     
 
 
--- tokenize_in_c.c
+++ tokenize_in_c.c
@@ -43,13 +43,73 @@ void revemove_delimeter(char *ptr, int len ){
 
 }
 
+void declare_result(char *ptr , int len){
+    char first_num[5];
+    char second_num[5];
+    int count_1 = 0 ;
+    int count_2 = 0 ;
+    int idx ; 
+    for(int i = 0 ; i<len ;i++){
+        if(ptr[i]=='-'){
+            idx = i;
+            break;
+        
+            
+        }
+    }
+    
 
+
+
+    for( int i = 0 ; i<idx; i++){
+        first_num[i]=ptr[i];
+        count_1+=1 ; 
+
+    }
+
+    first_num[count_1]='\0';
+
+
+    for(int i = idx+1 ; i<len ; i++){
+        second_num[count_2]=ptr[i];
+        count_2+=1;
+    }
+
+    second_num[count_2]='\0';
+
+    printf("%c\n", first_num[0]);
+    printf("%s\n", first_num);
+    printf("%s\n",second_num);
+    int num1 = atoi(first_num);
+    int num2 = atoi(second_num);
+    if(num1>num2){
+        int diff = num1-num2;
+        printf("Ronaldo's team has won the match by %d",diff);
+    }
+    else if(num1<num2){
+        int dif = num2-num1;
+        printf("Ronaldo's team has won the game %d",dif);
+    }
+    else{
+        printf("It's a draw !!");
+    }
+}
 
 
 int main(){
     char s[] = "I.Love.python";
     int len = sizeof(s)/sizeof(s[0]);
     revemove_delimeter(s , len);
+
+    // taking an input from the user ; 
+
+    char score[10];
+
+    printf("ENter The score of Realmadrid and bercalona : ");
+    scanf("%s",score);
+    int len2 =strlen(score);
+    declare_result(score , len2+1);
+
 
 
 
